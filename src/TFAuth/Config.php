53877ed4d0c36d6b890bca1183cc8d0070fb01ec
Chris Cornutt <enygma@phpdeveloper.org>
2013-03-16 07:12:06 -0500

    adding magic getter and docblock to Config class

--- src/TFAuth/Config.php
+++ src/TFAuth/Config.php
@@ -19,6 +19,21 @@ abstract class Config
         }
     }
 
+    /**
+     * Allow for the fetching of config options as properties
+     * @param string $name Config key name
+     * @return mixed If found, the config value. If not null
+     */
+    public function __get($name)
+    {
+        return (isset($this->config[$name])) ? $this->config[$name] : null;
+    }
+
+    /**
+     * Set the configuration of the current instance
+     * @param array $config Configuration settings
+     * @return \TFAuth\Config instance
+     */
     public function setConfig($config)
     {
         if (!is_array($config)) {
@@ -29,8 +44,14 @@ abstract class Config
         } else {
             $this->config = $config;
         }
+        return $this;
     }
 
+    /**
+     * Get either the entire set of config settings or a single value
+     * @param string $key Configuration key to try to locate [optional]
+     * @return array|string Either the one value if key is found, otherwise all settings
+     */
     public function getConfig($key = null)
     {
         return ($key !== null && isset($this->config[$key]))
